1a6eaacf8d3f7622a99e9630244bbe869951f68c
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-10-28 13:44:25 +0900

    Add DB config

--- db/db.go
+++ db/db.go
@@ -3,11 +3,12 @@ package db
 import (
 	"fmt"
 
-	"github.com/hiroyaonoe/le4db-go/config"
+	"github.com/hiroyaonoe/le4-db-go/config"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
-func NewDB() (*DB, error) {
+func NewDB() (*sqlx.DB, error) {
 	db, err := sqlx.Connect("postgres", config.DSN())
 	if err != nil {
 		return nil, fmt.Errorf("failed to open PostgreSQL: %w", err)
