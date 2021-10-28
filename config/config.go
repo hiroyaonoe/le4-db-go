da06582d9a2b8a26fcb934d697ab60442e51b852
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-10-28 13:44:02 +0900

    Add config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,10 @@
 package config
+
+import "os"
+
+func DSN() string {
+	return "user=" + os.Getenv("DB_USER") + 
+	" password=" + os.Getenv("DB_PASSWORD") + 
+	" dbname=" + os.Getenv("DB_NAME") + 
+	" sslmode=disable"
+}
