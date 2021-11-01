78b98f5ffbfbf22842de4ed1e0f917ab84d7e1b2
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-11-02 07:36:24 +0900

    ログイン処理を実装

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+    "github.com/gin-contrib/sessions"
+    "github.com/gin-contrib/sessions/cookie"
 	"github.com/hiroyaonoe/le4-db-go/config"
+	"github.com/hiroyaonoe/le4-db-go/pkg/auth"
+	"github.com/hiroyaonoe/le4-db-go/pkg/login"
 	"github.com/hiroyaonoe/le4-db-go/pkg/index"
 	"github.com/hiroyaonoe/le4-db-go/pkg/thread"
-	"github.com/hiroyaonoe/le4-db-go/pkg/auth"
 	"github.com/hiroyaonoe/le4-db-go/pkg/user"
 )
 
 func main() {
 	e := gin.Default()
+
+    store := cookie.NewStore([]byte("secret"))
+    e.Use(sessions.Sessions("mysession", store))
+
 	e.LoadHTMLGlob("view/*.html")
 
 	e.GET("", index.Get)
 
+	e.GET("/login", login.Get)
+	e.POST("/login", login.Post)
 	e.GET("/signup", auth.Signup)
 
 	u := e.Group("/user")
---
+++ pkg/login/get.go
@@ -0,0 +1,11 @@
+package login
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Get(c *gin.Context) {
+	c.HTML(http.StatusOK, "auth_login.html", gin.H{})
+}
