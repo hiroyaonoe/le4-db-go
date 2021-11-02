b564d915801b6b8b1de7041400572a45fe54a5dc
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-11-02 09:07:55 +0900

    スレッド作成にユーザー情報を追加

--- pkg/thread/get.go
+++ pkg/thread/get.go
@@ -1,6 +1,7 @@
 package thread
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -12,21 +13,29 @@ func Get(c *gin.Context) {
 	db, err := db.NewDB()
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	threadID, err := strconv.Atoi(c.Param("thread_id"))
 	if err != nil {
 		c.String(http.StatusBadRequest, "bad request")
+		return
 	}
 
 	threads := []Thread{}
-	err = db.Select(&threads, "SELECT thread_id, title FROM threads WHERE thread_id = $1", threadID)
+	err = db.Select(&threads, "SELECT thread_id, title, created_at, users.name AS user_name FROM threads NATURAL JOIN post_threads NATURAL JOIN users WHERE thread_id = $1", threadID)
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	if len(threads) == 0 {
+		c.String(http.StatusNotFound, fmt.Sprintf("thread %d not found", threadID))
+		return
 	}
 	thread := threads[0]
 
 	c.HTML(http.StatusOK, "thread_get.html", gin.H{
 		"thread": thread,
+		"createdAt": thread.CreatedAt.Local().Format("2006/01/02 15:04:05"),
 	})
 }
--- pkg/thread/thread.go
+++ pkg/thread/thread.go
@@ -1,6 +1,11 @@
 package thread
 
+import "time"
+
 type Thread struct {
-	ThreadID int    `db:"thread_id"`
-	Title    string `db:"title"`
+	ThreadID  int       `db:"thread_id"`
+	Title     string    `db:"title"`
+	UserID    int       `db:"user_id"`
+	UserName  string    `db:"user_name"`
+	CreatedAt time.Time `db:"created_at"`
 }
