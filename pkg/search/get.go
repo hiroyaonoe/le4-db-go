8457a339b68a6394efa4848a14cf171756e33047
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-11-02 22:44:05 +0900

    カテゴリ検索を追加

--- pkg/search/get.go
+++ pkg/search/get.go
@@ -3,10 +3,12 @@ package search
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/hiroyaonoe/le4-db-go/db"
+	"github.com/hiroyaonoe/le4-db-go/pkg/category"
 	"github.com/hiroyaonoe/le4-db-go/pkg/comment"
 	"github.com/hiroyaonoe/le4-db-go/pkg/thread"
 )
@@ -20,25 +22,53 @@ func Get(c *gin.Context) {
 
 	searchQuery := c.Query("query")
 	words := strings.Fields(searchQuery)
-	wordsI := make([]interface{}, len(words))
-	for i, v := range words {
-		wordsI[i] = "%"+v+"%"
-	}
 
-	query := "SELECT thread_id, title, created_at, user_id, users.name AS user_name " + 
+	
+	searchCategory := c.Query("category")
+
+	query := "SELECT thread_id, title, created_at, user_id, users.name AS user_name, categories.category_id, categories.name AS category_name " + 
 		"FROM threads " + 
 		"NATURAL JOIN post_threads " + 
-		"NATURAL JOIN users"
-	if len(words) > 0 {
-		likeQuery := make([]string, len(words))
-		for i := 0; i < len(words); i++ {
-			likeQuery[i] = fmt.Sprintf("title LIKE $%d", i+1)
+		"NATURAL JOIN users " + 
+		"NATURAL JOIN link_categories " + 
+		"JOIN categories ON categories.category_id = link_categories.category_id "
+
+	var args []interface{}
+	categoryID, err := strconv.Atoi(searchCategory)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+	if categoryID >= 0 {
+		query = query + "WHERE categories.category_id = $1 "
+		args = make([]interface{}, len(words)+1)
+		args[0] = categoryID
+		for i, v := range words {
+			args[i+1] = "%"+v+"%"
 		}
-		query = query + " WHERE " + strings.Join(likeQuery, " OR ")
+		if len(words) > 0 {
+			likeQuery := make([]string, len(words))
+			for i := 0; i < len(words); i++ {
+				likeQuery[i] = fmt.Sprintf("title LIKE $%d", i+2)
+			}
+			query = query + "AND " + strings.Join(likeQuery, " OR ")
+		}
+	} else { // searchCategory == all の場合
+		args = make([]interface{}, len(words))
+		for i, v := range words {
+			args[i] = "%"+v+"%"
+		}
+		if len(words) > 0 {
+			likeQuery := make([]string, len(words))
+			for i := 0; i < len(words); i++ {
+				likeQuery[i] = fmt.Sprintf("title LIKE $%d", i+1)
+			}
+			query = query + "WHERE " + strings.Join(likeQuery, " OR ")
+		}
 	}
 
 	threads := []thread.Thread{}
-	err = db.Select(&threads, query, wordsI...)
+	err = db.Select(&threads, query, args...)
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
 		return
@@ -48,17 +78,47 @@ func Get(c *gin.Context) {
 		"FROM post_comments " + 
 		"JOIN comments ON post_comments.thread_id = comments.thread_id AND post_comments.comment_id = comments.comment_id " + 
 		"JOIN threads ON comments.thread_id = threads.thread_id " + 
-		"JOIN users ON post_comments.user_id = users.user_id"
-	if len(words) > 0 {
-		likeQuery := make([]string, len(words))
-		for i := 0; i < len(words); i++ {
-			likeQuery[i] = fmt.Sprintf("comments.content LIKE $%d", i+1)
+		"JOIN users ON post_comments.user_id = users.user_id " + 
+		"JOIN link_categories ON link_categories.thread_id = threads.thread_id " + 
+		"JOIN categories ON categories.category_id = link_categories.category_id "
+	
+	if categoryID >= 0 {
+		query = query + "WHERE categories.category_id = $1 "
+		args = make([]interface{}, len(words)+1)
+		args[0] = categoryID
+		for i, v := range words {
+			args[i+1] = "%"+v+"%"
 		}
-		query = query + " WHERE " + strings.Join(likeQuery, " OR ")
+		if len(words) > 0 {
+			likeQuery := make([]string, len(words))
+			for i := 0; i < len(words); i++ {
+				likeQuery[i] = fmt.Sprintf("comments.content LIKE $%d", i+2)
+			}
+			query = query + "AND " + strings.Join(likeQuery, " OR ")
+		}
+	} else { // searchCategory == all の場合
+		args = make([]interface{}, len(words))
+		for i, v := range words {
+			args[i] = "%"+v+"%"
+		}
+		if len(words) > 0 {
+			likeQuery := make([]string, len(words))
+			for i := 0; i < len(words); i++ {
+				likeQuery[i] = fmt.Sprintf("comments.content LIKE $%d", i+1)
+			}
+			query = query + "WHERE " + strings.Join(likeQuery, " OR ")
+		}
 	}
 
 	comments := []comment.Comment{}
-	err = db.Select(&comments, query, wordsI...)
+	err = db.Select(&comments, query, args...)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	
+	categories := []category.Category{}
+	err = db.Select(&categories, "SELECT category_id, name FROM categories")
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
 		return
@@ -67,6 +127,7 @@ func Get(c *gin.Context) {
 	c.HTML(http.StatusOK, "search.html", gin.H{
 		"threads": threads,
 		"comments": comments,
-		"query": searchQuery,
+		"categoryID": categoryID,
+		"categories": categories,
 	})
 }
