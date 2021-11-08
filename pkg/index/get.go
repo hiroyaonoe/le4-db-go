6c0e7334dfca6832ab562436ccae3bcba1cb2025
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-11-09 08:05:51 +0900

    indexのスレッドを10個に制限

--- pkg/index/get.go
+++ pkg/index/get.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hiroyaonoe/le4-db-go/db"
 	"github.com/hiroyaonoe/le4-db-go/domain"
 	"github.com/hiroyaonoe/le4-db-go/pkg/auth"
+	"github.com/jmoiron/sqlx"
 )
 
 func Get(c *gin.Context) {
@@ -17,7 +18,7 @@ func Get(c *gin.Context) {
 	}
 
 	threads := []domain.Thread{}
-	err = db.Select(&threads, "SELECT thread_id, title, created_at, user_id, user_name, category_id, category_name FROM threads_with_user_category")
+	err = db.Select(&threads, "SELECT thread_id, title, created_at, user_id, user_name, category_id, category_name FROM threads_with_user_category LIMIT 10")
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
 		return
@@ -38,14 +39,24 @@ func Get(c *gin.Context) {
 	}
 
 	AddTags := []domain.Tag{}
-	err = db.Select(&AddTags, "SELECT tag_id, name, thread_id FROM tag_with_thread_id")
+	query := "SELECT tag_id, name, thread_id FROM tag_with_thread_id WHERE thread_id IN (:thread_id)"
+	query, args, err := sqlx.Named(query, threads)
+	query, args, err = sqlx.In(query, args)
+	query = db.Rebind(query)
+	err = db.Select(&AddTags, query, args...)
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	numComments := []domain.Thread{}
-	err = db.Select(&numComments, "SELECT thread_id, num_comment FROM num_comments")
+	query = "SELECT thread_id, num_comment " +
+		"FROM num_comments " +
+		"WHERE thread_id IN (:thread_id)"
+	query, args, err = sqlx.Named(query, threads)
+	query, args, err = sqlx.In(query, args)
+	query = db.Rebind(query)
+	err = db.Select(&numComments, query, args...)
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
 		return
