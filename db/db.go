49ce9f51fb1ab75907d53c5e2226ef1fe2520101
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-11-09 14:24:33 +0900

    sqlのlogを追加

--- db/db.go
+++ db/db.go
@@ -1,19 +1,26 @@
 package db
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/hiroyaonoe/le4-db-go/config"
 	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
+	"github.com/rs/zerolog"
+	sqldblogger "github.com/simukti/sqldb-logger"
+	"github.com/simukti/sqldb-logger/logadapter/zerologadapter"
 )
 
 func NewDB() (*sqlx.DB, error) {
-	db, err := sqlx.Connect("postgres", config.DSN())
-	if err != nil {
-		return nil, fmt.Errorf("failed to open PostgreSQL: %w", err)
-	}
-
+	logger := zerolog.New(
+		zerolog.ConsoleWriter{Out: os.Stdout, NoColor: false},
+	)
+	dbrow := sqldblogger.OpenDriver(
+		config.DSN(),
+		pq.Driver{},
+		zerologadapter.New(logger),
+	)
+	db := sqlx.NewDb(dbrow, "postgres")
 	db.SetMaxIdleConns(100)
 	db.SetMaxOpenConns(100)
 
